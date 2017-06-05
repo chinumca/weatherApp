0c6707de2e46d762177759612b9493b691a69550
sachinpatilmca <chinu50patil@gmail.com>
2017-06-05 10:12:42 +0530

    Delete GridView.spec.js

--- site/js/GridView.spec.js
+++
@@ -1,8 +0,0 @@
-import render from './GridView';
-
-describe ('render',()=>{
-    it('should update dom',()=>{
-        const result=render([{name: "usdjpy",bestBid: 106.7297012204255,bestAsk: 107.25199883791178,openBid: 107.22827132623534,openAsk: 109.78172867376465,lastChangeAsk: -4.862314256927661,lastChangeBid: -2.8769211401569663}]);
-        expect(result).tobe(0);
-    })
-})
